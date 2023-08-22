4f968079345cc38b9c62a91941c179ccc4b93444
Masibulele <masibulele100@gmail.com>
2023-08-22 09:43:25 +0200

    included port number

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import smtplib
 email= os.getenv('EMAIL')
 email_password= os.getenv('EMAIL_PASSWORD')
+receiver= os.getenv('RECEIVER_EMAIL')
 app= Flask(__name__)
 
 
@@ -25,22 +26,23 @@ def contact_page():
 def get_form_data():
     if request.method=="POST":
         sender_name= request.form['name']
-        sender_email= request.form['email']
+        sender_mail= request.form['email']
         sender_tel= request.form['telephone']
         sender_text= request.form['text']
-        send_email(name=sender_name,sender_email=sender_email,tel=sender_tel,text=sender_text)
+        send_email(name=sender_name,sender_email=sender_mail,tel=sender_tel,text=sender_text)
         return render_template('contact.html')
 
     return render_template('index.html')
 
 def send_email(name,sender_email,tel,text):
     email_message= f"Subject: New Message\n\n Name: {name}\n Email: {sender_email}\n Telephone: {tel}\n Message: {text}"
-    with smtplib.SMTP('smtp.gmail.com') as connection:
+    with smtplib.SMTP('smtp.gmail.com','587') as connection:
         my_email=email
         my_password = email_password
+        to_address= receiver
         connection.starttls()
         connection.login(my_email,my_password)
-        connection.sendmail(my_email,my_email,msg=email_message)
+        connection.sendmail(my_email,to_address,msg=email_message)
 
 
 if __name__ == '__main__':
